4b8f93515c4553c221b668cf09b60463cf302c3d
hudk <hdkwd@126.com>
2018-03-02 14:38:09 +0800

    add dialog

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Button from '../lib/button/button';
 import Table from '../lib/table/table';
 import TableEdit from '../lib/table/table-edit';
 import Datepicker from '../lib/datepicker/datepicker';
+import Dialog from '../lib/dialog/dialog';
 
 const install = function (Vue) {
   Vue.component('h-album-list', AlbumList);
@@ -10,6 +11,7 @@ const install = function (Vue) {
   Vue.component('h-table', Table);
   Vue.component('h-table-edit', TableEdit);
   Vue.component('h-datepicker', Datepicker);
+  Vue.component('h-dialog', Dialog);
   Vue.component('h-album-list', AlbumList);
   // Vue.component('h-upload', UploadCore)
 }
